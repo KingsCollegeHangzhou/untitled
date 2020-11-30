4625671952bf881518b56595439a27f578e9531c
mario.pons <llucop@gmail.com>
2020-11-30 10:05:44 +0800

    Added some functionality

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
+<<<<<<< HEAD
 print("Hi there!")
+=======
+#Testing file for Git vcs
+#Date: 30.11.2020
+
+
+
+
+name = input("Enter your name")
+
+print("Hi there {0}!".format( name))
+>>>>>>> Initial commit
